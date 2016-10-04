e5b176662d13e5ce201125668a721778f53c646c
John W. Long <john@uservoice.com>
2016-10-04 17:54:55 -0400

    Handle multiple arguments

--- hello.c
+++ hello.c
@@ -3,10 +3,14 @@
 
 int main(int argc, char *argv[])
 {
-  if (argc > 1) {
-    printf("Hello %s!\n", argv[1]);
-  } else {
+  int code = 0;
+  if (argc == 1) {
     printf("Hello CloudBees!\n");
+  } else if (argc == 2) {
+    printf("Hello %s!\n", argv[1]) ;
+  } else {
+    printf("hello: too many parameters\n");
+    code = 1;
   }
-  return 0;
+  return code;
 }

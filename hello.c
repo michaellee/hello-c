e0e5deb131576863ac1b1cf25b6b6725c4165350
Michael Lee <mlee@cloudbees.com>
2017-10-26 09:59:20 -0400

    Changed message to CloudBees

--- hello.c
+++ hello.c
@@ -4,9 +4,9 @@
 int main(int argc, char *argv[])
 {
   if (argc > 1) {
-    printf("Hello %s!\n", argv[1]) ;
+    printf("Hello %s!\n", argv[1]);
   } else {
-    printf("Hello world!\n");
+    printf("Hello CloudBees!\n");
   }
   return 0;
 }
